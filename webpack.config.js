41af1187d5d4deaae390334ac3d011059068b55e
Eric <dengyangself@163.com>
2017-09-27 13:21:54 +0800

    npm install file-loader

--- webpack.config.js
+++ webpack.config.js
@@ -27,8 +27,15 @@ const config = {
                 use: 'ts-loader'
             },
             {
-                test: /\.(js|jsx)$/,
-                loader: "babel-loader"
+                test: /\.(png|svg|jpg|gif)$/,
+                loader: "file-loader"
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
             }
         ]
     },
