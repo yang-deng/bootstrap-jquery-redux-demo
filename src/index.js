39f46d8e9c6d883a8eeb6861bbb86104fd67c8ec
Eric <dengyangself@163.com>
2017-09-29 16:11:13 +0800

    lazy loading

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
-function getComponent() {
-    return import (
-        /* webpackChunkName: "lodash" */
-        'lodash').then(_ => {
-        var element = document.createElement('div');
+import _ from 'lodash';
 
-        element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+function component() {
+    var element = document.createElement('div');
 
-        return element;
-    }).catch(error => 'An error occured whild loading the component');
+    var button = document.createElement('button');
+    var br = document.createElement('br');
+
+    button.innerHTML = 'Click me and look at the console!';
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.appendChild(br);
+    element.appendChild(button);
+
+    button.onclick = e =>
+        import ( /* webpackChunkName: "print" */ './print').then(
+            module => {
+                var print = module.default;
+                print();
+            }
+        );
+
+    return element;
 }
 
-getComponent().then(component => {
-    document.body.appendChild(component);
-})
+document.body.appendChild(component());
